0b0f4caa4b2161b831a3f930b252ccd48c0e6e11
Chen Weizhi <d2d.weizhi@gmail.com>
2025-04-05 21:32:59 +0800

    Got the weather app sample working.

--- ch06_fetching_data/weather_app/lib/screens/weather_data_screen.dart
+++ ch06_fetching_data/weather_app/lib/screens/weather_data_screen.dart
@@ -26,29 +26,83 @@ class _WeatherDataScreenState extends State<WeatherDataScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
-
+    _getLocation();
   }
 
   // Method to get device location
-  Future<void> _getLocation async {
+  Future<void> _getLocation() async {
     try {
       // Create an instance of LocationService
       LocationService location = LocationService();
       // Get the current device location
       _position = await location.getLocation();
 
+      // Fetch weather data using device location.
+      final weatherData = await fetchWeather(
+        _position?.latitude ?? 0.0,
+        _position?.longitude ?? 0.0
+      );
 
+      // Navigate to the result screen if weather data is available
+      if (weatherData != null) {
+        _navigateToWeatherResult(weatherData);
+      }
     } catch (e) {
       print('Error: $e');
+    }
   }
 
+  // Method to fetch weather data from API
   Future<WeatherData?> fetchWeather(double latitude, double longitude) async {
+    try {
+      const apiKey = 'd7c95d67cdde7cdda01266293cfac424';
+      final apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric';
 
+      // Make a GET request to fetch weather data
+      final response = await http.get(Uri.parse(apiUrl));
+
+      if (response.statusCode == 200) {
+        // Parse JSON response and create WeatherData object
+        final jsonMap = json.decode(response.body);
+        final weatherData = WeatherData.fromJson(jsonMap);
+        return weatherData;
+      } else {
+        throw Exception('Failed to load weather data');
+      }
+    } catch (e) {
+      print('Error: $e');
+      return null;
+    }
   }
-}
+
+  void _navigateToWeatherResult(WeatherData weatherData) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+          builder: (context) => WeatherResultScreen(weatherData: weatherData),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        title: const Text("Weather Data"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget> [
+            // Display device latitude and longitude
+            Center(
+              child: _position != null
+                ? Text("Latitude: ${_position?.latitude}, Longitude: ${_position?.longitude}")
+                : const CircularProgressIndicator(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- ch06_fetching_data/weather_app/lib/services/location.dart
+++ ch06_fetching_data/weather_app/lib/services/location.dart
@@ -3,10 +3,12 @@ import 'package:geolocator/geolocator.dart';
 class LocationService {
   Future<Position?> getLocation() async {
     try {
-      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+      print('Position: ${position.latitude}, ${position.longitude}');
       return position;
     } catch (e) {
       print("Error getting location: $e");
     }
+    return null;
   }
 }
